238f5114b3bded02a30793db28a96490cde0a051
Lorenzo <107998561+lorenzoworx@users.noreply.github.com>
2023-08-15 10:08:14 +0100

    ADD MusicAlbum class properties visible in UML diagram

--- music_album.rb
+++ music_album.rb
@@ -1,4 +1,16 @@
 require_relative 'item'
 
-class MusicAlbum
+class MusicAlbum < item
+  attr_reader :on_spotify, :archived
+
+  def initialize(on_spotify: false, archived: false)
+    super(archived: archived)
+    @on_spotify = on_spotify
+  end
+
+  private
+
+  def can_be_archived?
+    @archived && @on_spotify
+  end
 end
