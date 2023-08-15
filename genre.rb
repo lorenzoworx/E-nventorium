e9086ace85ad075215834bbae83133a9b2a17faa
Lorenzo <107998561+lorenzoworx@users.noreply.github.com>
2023-08-15 10:34:18 +0100

    ADD all Genre class properties as seen on the UML diagram

--- genre.rb
+++ genre.rb
@@ -1,4 +1,13 @@
 require_relative './item'
 
 class Genre
+  def initialize(name)
+    @name = name
+    @items = []
+  end
+
+  def add_item=(item)
+    item.genre = self
+    @items << item
+  end
 end
