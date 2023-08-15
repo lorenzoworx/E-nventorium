03579f4a4ea8bb931b33e629e9e4a4044230f625
wolterbwambale <benplaan1@gmail.com>
2023-08-15 14:27:47 +0300

    update the item class

--- item.rb
+++ item.rb
@@ -4,33 +4,30 @@ class Item
   attr_reader :id, :archived
   attr_accessor :publish_date
 
-  def initialize(id, publish_date)
+  def initialize(_id, publish_date)
     @id = rand(1..1000)
     @publish_date = publish_date
     @archived = false
   end
 
   def can_be_archived?
-    !@archived
+    years_difference = years_since_publish_date
+    years_difference >= 10
   end
 
   def move_to_archive
+    return unless can_be_archived?
+
     @archived = true
   end
 
-  def genre=(genre)
-    @genre = genre
-  end
+  attr_writer :genre, :author, :source, :label
 
-  def author=(author)
-    @author = author
-  end
+  private
 
-  def source=(source)
-    @source = source
-  end
-  
-  def label=(label)
-    @label = label
+  def years_since_publish_date
+    current_date = Date.today
+    publish_date = Date.parse(@publish_date)
+    current_date.year - publish_date.year
   end
 end
