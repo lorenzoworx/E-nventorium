9a383ae300d98c28e73593d4655a574e57cd4f77
wolterbwambale <benplaan1@gmail.com>
2023-08-16 15:43:46 +0300

    add logic for add new game,list of author

--- app.rb
+++ app.rb
@@ -1,3 +1,7 @@
+require_relative 'classes/game'
+require_relative 'classes/item'
+require_relative 'classes/author'
+
 class App
   OPTIONS = {
     '1' => :list_all_books,
@@ -12,6 +16,11 @@ class App
     '10' => :add_a_music_album,
     '11' => :add_a_game
   }.freeze
+
+  def initialize
+    @authors = []
+    @games = []
+  end
 
   def run(option)
     send(OPTIONS[option])
@@ -30,7 +39,10 @@ class App
   end
 
   def list_all_games
-    puts 'List of games'
+    puts 'List of games:'
+    @games.each_with_index do |game, index|
+      puts "#{index + 1}. #{game.title} (#{game.multiplayers} players), Last Played: #{game.lastplayed_date}"
+    end
   end
 
   def list_all_genres
@@ -42,7 +54,16 @@ class App
   end
 
   def list_all_authors
-    puts 'List of authors'
+    puts 'List of authors:'
+    @authors.each do |author|
+      puts "#{author.firstname} #{author.lastname} (ID: #{author.id})"
+      author.items.each do |item|
+        if item.is_a?(Game)
+          puts " - Game: #{item.title} (Players: #{item.multiplayers}), Last Played: #{item.lastplayed_date}"
+          # Add similar logic for other item types (books, music albums, etc.)
+        end
+      end
+    end
   end
 
   def list_all_sources
@@ -57,7 +78,53 @@ class App
     puts 'Add a new music album'
   end
 
+  def add_game(game)
+    @games << game
+  end
+
+  def find_or_create_author(firstname, lastname)
+    existing_author = @authors.find { |author| author.firstname == firstname && author.lastname == lastname }
+    if existing_author
+      existing_author
+    else
+      new_author = Author.new(firstname, lastname)
+      @authors << new_author
+      new_author
+    end
+  end
+
   def add_a_game
     puts 'Add a new game'
+    print 'Enter the game title: '
+    game_title = gets.chomp
+
+    print 'Enter the number of players: '
+    num_players = gets.chomp.to_i
+
+    print 'Enter the last played date (YYYY-MM-DD): '
+    last_played_date = gets.chomp
+
+    new_game = Game.new(num_players, last_played_date)
+    new_game.title = game_title
+    
+    puts 'Add a new author'
+    print 'Enter the author\'s first name: '
+    firstname = gets.chomp
+    print 'Enter the author\'s last name: '
+    lastname = gets.chomp
+
+    existing_author = @authors.find { |author| author.firstname == firstname && author.lastname == lastname }
+    if existing_author
+      new_author = existing_author
+    else
+      new_author = Author.new(firstname, lastname)
+      @authors << new_author
+    end
+
+    new_author.add_item(new_game)
+
+    add_game(new_game)
+
+    puts "Added #{game_title} to the list of games."
   end
 end
