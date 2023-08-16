8b15b6a8c1d6f2c46af30787ad81e54ff7689395
wolterbwambale <benplaan1@gmail.com>
2023-08-16 15:44:02 +0300

    add author class

---
+++ classes/author.rb
@@ -0,0 +1,15 @@
+class Author
+  attr_reader :id, :firstname, :lastname
+  attr_accessor :items
+
+  def initialize(firstname, lastname)
+    @id = rand(1..1000)
+    @firstname = firstname
+    @lastname = lastname
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item
+  end
+end
